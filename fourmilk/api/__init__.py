d0259a4a32c25f9358039633716d8803eac0330b
Yufei Li <yufeiminds@gmail.com>
2015-11-13 20:34:23 +0800

    Finish user login

--- fourmilk/api/__init__.py
+++ fourmilk/api/__init__.py
@@ -2,18 +2,33 @@
 
 from flask.views import MethodView
 from flask import jsonify
-from fourmilk import app, config, logger
 from fourmilk.models import mongo
+
+from webargs import fields
+from webargs.flaskparser import use_args
+
+login_args = {
+    "username": fields.Str(require=True),
+    "password": fields.Str(require=True)
+}
 
 
 class PartView(MethodView):
     route = '/part'
 
-    def get(self):
-        return jsonify(a=1)
+    @use_args(login_args)
+    def login(self, args):
+        user = mongo.part.find_one(args)
+        user.pop('_id')
+        if user:
+            return jsonify(user), 200
+        else:
+            return jsonify(status=200, message=None), 401
 
-    def hello(self):
-        return jsonify(mongo.part.find())
+    @use_args(login_args)
+    def register(self, args):
+        status = mongo.part.insert(args)
+        return jsonify(data=str(status))
 
 
 __all__ = [
